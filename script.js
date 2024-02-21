90f8d0bab56b49670429a0daffe19e176e9ed05d
Shubham9528 <81708211+Shubham9528@users.noreply.github.com>
2024-02-22 02:03:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".btn1-close").click(function(){
+    $('.side-bar').addClass('hide');
+});
+$(".btn2-close").click(function(){
+    $('.side-bar').removeClass('hide').css("transition","transform 0.5s");
+
+});
+
+
+
+
